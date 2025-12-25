b552e833d41e0097aecab0306bbc13cdfc65c527
bibek1414 <bbhattarai770@gmail.com>
2025-12-25 15:06:04 +0545

    style: reduce horizontal padding on the home page section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import HeroSection from "@/components/HeroSeciton";
 const HomePage: React.FC = () => {
   return (
-    <section className="flex min-h-screen items-center justify-center bg-white text-center px-4">
+    <section className="flex min-h-screen items-center justify-center bg-white text-center px-2">
 
       <HeroSection />
     </section>
